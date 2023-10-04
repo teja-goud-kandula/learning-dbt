c55ed241bc6f5392e87a45c2a99251281bbcb30c
Teja Goud Kandula <kandulateja6@gmail.com>
2023-10-04 07:19:03 +0530

    Using a for loop in Jinja

--- project1/models/marts/core/JinjaTemplate.sql
+++ project1/models/marts/core/JinjaTemplate.sql
@@ -10,3 +10,8 @@
 
 -- accessing values from the list 
 -- select ' {{ my_animals[0] }} ' as animal 
+
+-- parsing over the list using a for loop
+{% for animal in my_animals %}
+    My favoriteee animal is the  {{ animal }}
+{% endfor %}
