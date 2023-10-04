93262b5e44d668e33182ad57c09e3a88b87fd9aa
Teja Goud Kandula <kandulateja6@gmail.com>
2023-10-04 07:18:27 +0530

    Using a list in Jinja

--- project1/models/marts/core/JinjaTemplate.sql
+++ project1/models/marts/core/JinjaTemplate.sql
@@ -4,3 +4,9 @@
 
 -- accessing the set variable 
 {{ my_cool_string }}
+
+-- set values in a list 
+{% set my_animals = ['lemur', 'wolf', 'panther', 'lion'] %}
+
+-- accessing values from the list 
+-- select ' {{ my_animals[0] }} ' as animal 
