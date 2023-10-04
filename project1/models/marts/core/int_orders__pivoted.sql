90d57af4665d99e3754070bb4005981d056becc5
Teja Goud Kandula <kandulateja6@gmail.com>
2023-10-04 09:04:11 +0530

    Refactored the code for readability

--- project1/models/marts/core/int_orders__pivoted.sql
+++ project1/models/marts/core/int_orders__pivoted.sql
@@ -1,19 +1,19 @@
+{%- set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] -%}
+
 with payments as (
     select * from {{ ref('stg_payments') }}
 ),
 
 pivoted as (
     select 
+        
         order_id, 
-
-        {% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] -%}
-
-        {%- for payment_method in payment_methods -%}
-            sum(case when payment_method = ' {{payment_method}} ' then amount else 0 end ) as {{payment_method}}_amount 
-            {%- if not loop.last -%}
-                ,
-            {% endif %}
-        {% endfor %}
+        {% for payment_method in payment_methods -%}
+        sum(case when payment_method = ' {{payment_method}} ' then amount else 0 end ) as {{payment_method}}_amount 
+        {%- if not loop.last -%}
+            ,
+        {% endif -%}
+        {%- endfor %}
         
     from payments 
     group by 1
